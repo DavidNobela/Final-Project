e90b778dd96b2887509dbe9c1fe130fea828d995
David <DaNobela1@s.sfusd.edu>
2024-05-14 14:30:44 -0700

    Dying 2.0

--- scripts/Killzone.gd
+++ scripts/Killzone.gd
@@ -5,9 +5,12 @@ extends Area2D
 func _on_body_entered(body):
 	#pass # Replace with function body.
 	print("You Died")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	#pass # Replace with function body.
 	get_tree().reload_current_scene()
