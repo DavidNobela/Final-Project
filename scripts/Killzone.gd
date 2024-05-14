af82096fd16e8df6c3e907cabb1648ee554fd1dc
0Nott <jixiao1@s.sfusd.edu>
2024-05-14 13:43:17 -0700

    Dying 1.0 finished

---
+++ scripts/Killzone.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+func _on_body_entered(body):
+	#pass # Replace with function body.
+	print("You Died")
+	timer.start()
+
+
+func _on_timer_timeout():
+	#pass # Replace with function body.
+	get_tree().reload_current_scene()
