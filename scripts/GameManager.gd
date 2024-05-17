f92bd348e016975e11f58357f4f2c9582f3c8c2f
David <DaNobela1@s.sfusd.edu>
2024-05-17 15:42:49 -0700

    Score Finish

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -2,7 +2,9 @@ extends Node
 
 var score = 0
 
+@onready var score_label = $ScoreLabel
+
 
 func add_point():
 	score += 1
-	print(score)
+	score_label.text = "You collected " + str(score) + " coins."
