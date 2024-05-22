a2d9c4ac1af8cf8518283ed30d80f2475b50ebb1
David <DaNobela1@s.sfusd.edu>
2024-05-22 11:04:00 -0700

    Death sound effect

--- scripts/Killzone.gd
+++ scripts/Killzone.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var death_sound_player = $DeathSoundPlayer
 
 func _on_body_entered(body):
 	#pass # Replace with function body.
@@ -8,7 +9,7 @@ func _on_body_entered(body):
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
-
+	death_sound_player.play()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1.0
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -400.0
 @onready var sfx_jump = $sfx_jump
 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -19,6 +20,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		sfx_jump.play()
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
