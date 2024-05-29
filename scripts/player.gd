a191740676aeffa767a991650c5a9597ca4db639
0Nott <jixiao1@s.sfusd.edu>
2024-05-28 22:09:06 -0700

    coyote jump and double jump unfixed

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,21 +7,39 @@ const JUMP_VELOCITY = -400.0
 var Jump_Avalible: bool = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_count = 0
+var max_jumps = 2
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var coyote_jump_timer = $CoyoteJumpTimer
+@onready var coyote_timer = $CoyoteTimer
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+		if is_on_floor():
+			jump_count = 0
+		#timer needed 
+		#deleted parts:
+		#if velocity.y>0:
+			#velocity.y -= Jump_gravity * delta
+		#else: 
+			#velocity.y -= Fall_Gravity * delta
+		
+	else:
+		Jump_Avalible = true
+		
+
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 	
 	if Input.is_action_pressed("Jump") and Jump_Avalible:
 		velocity.y = JUMP_VELOCITY
+		Jump_Avalible = false
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -46,3 +64,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout():
+	Jump_Avalible = false
+	pass # Replace with function body.
