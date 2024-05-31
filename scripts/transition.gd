a97a258ef952c439e9fe69f6211d35d3594c4d10
David <DaNobela1@s.sfusd.edu>
2024-05-30 23:25:41 -0700

    Fixed Transition

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -3,9 +3,9 @@ extends Node
 @onready var animation_player = $AnimationPlayer
 
 func fade_from_black():
-	animation_player.play("Fade from black")
+	animation_player.play("fade_from_black")
 	await animation_player.animation_finished
 
 func fade_to_black():
-	animation_player.play("Fade to black")
+	animation_player.play("fade_to_black")
 	await animation_player.animation_finished
