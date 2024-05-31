4b401a3e27f9ef3764a4f15c9c551af330e78e32
David <DaNobela1@s.sfusd.edu>
2024-05-30 18:45:55 -0700

    Transition for the start menu

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -7,8 +7,9 @@ func _ready():
 	start_game_button.grab_focus()
 
 func _on_start_game_button_pressed():
+	await Transition.fade_to_black()
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
-	
+	Transition.fade_from_black()
 	
 func _on_quit_buttom_pressed():
 	get_tree().quit()
